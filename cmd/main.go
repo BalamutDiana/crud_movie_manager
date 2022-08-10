ebef6081ff5d7ae0daf8612158ee8c1c2953c6a3
BalamutDiana <67107383+BalamutDiana@users.noreply.github.com>
2022-08-10 12:33:11 +0300

    divided functionality into packages
    functions and connection are placed in separate packages

--- cmd/main.go
+++ cmd/main.go
@@ -1,107 +1,31 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
 	"log"
-	"time"
 
+	movies "github.com/BalamutDiana/crud_movie_manager/internal"
+	db "github.com/BalamutDiana/crud_movie_manager/pkg/database"
 	_ "github.com/lib/pq"
 )
 
-type Movie struct {
-	ID               int64
-	Title            string
-	Release          string
-	StreamingService string
-	SavedAt          time.Time
-}
-
 func main() {
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
-		"password=%s dbname=%s sslmode=disable", "0.0.0.0", 5432, "postgres", "1marvin2mode3", "postgres")
-	db, err := sql.Open("postgres", psqlInfo)
+	db, err := db.NewPostgresConnection(db.ConnectionInfo{
+		Host:     "localhost",
+		Port:     5432,
+		Username: "postgres",
+		DBName:   "postgres",
+		SSLMode:  "disable",
+		Password: "1marvin2mode3",
+	})
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
 
-	if err := db.Ping(); err != nil {
-		log.Fatal(err)
-	}
-
-	// err = updateMovie(db, 2, Movie{
-	// 	Title:            "The Sandman",
-	// 	Release:          "2022",
-	// 	StreamingService: "Netflix",
-	// })
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// err = insertMovie(db, Movie{
-	// 	Title:            "Wheel of time",
-	// 	Release:          "2021",
-	// 	StreamingService: "Amazon",
-	// })
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	movies, err := GetMovies(db)
+	m, err := movies.GetMovieByID(db, 3)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(movies)
-
-}
-
-func GetMovies(db *sql.DB) ([]Movie, error) {
-	rows, err := db.Query("select * from movies")
-	if err != nil {
-		return nil, err
-	}
-	defer rows.Close()
-
-	movies := make([]Movie, 0)
-	for rows.Next() {
-		m := Movie{}
-		err := rows.Scan(&m.ID, &m.Title, &m.Release, &m.StreamingService, &m.SavedAt)
-		if err != nil {
-			return nil, err
-		}
-		movies = append(movies, m)
-	}
-	err = rows.Err()
-	if err != nil {
-		return nil, err
-	}
-
-	return movies, nil
-}
-
-func GetMovieByID(db *sql.DB, id int) (Movie, error) {
-	var m Movie
-	err := db.QueryRow("select * from movies where id = $1", id).
-		Scan(&m.ID, &m.Title, &m.Release, &m.StreamingService, &m.SavedAt)
-
-	return m, err
-}
-
-func insertMovie(db *sql.DB, m Movie) error {
-	_, err := db.Exec("insert into movies (title, release, streaming_service) values ($1, $2, $3)",
-		m.Title, m.Release, m.StreamingService)
-
-	return err
-}
-
-func deleteMovie(db *sql.DB, id int) error {
-	_, err := db.Exec("delete from movies where id = $1", id)
-	return err
-}
-
-func updateMovie(db *sql.DB, id int, newMovie Movie) error {
-	_, err := db.Exec("update movies set title=$1, release = $2, streaming_service = $3 where id = $4",
-		newMovie.Title, newMovie.Release, newMovie.StreamingService, id)
-	return err
+	fmt.Println(m)
 }
